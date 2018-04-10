b0150bb63ecc4c582efbeb3874d11ec9d4274184
Cong <2219332493@qq.com>
2018-04-10 22:15:37 +0800

    F1手机ajax数据生成

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,32 @@
-$(document).ready(function(){
-    $('.flexslider').flexslider({
-        directionNav: true,
-        pauseOnAction: false,
-        slideshowSpeed: 2000
-    });
+document.addEventListener('DOMContentLoaded',function(){
+    var data=(function(){
+        var jieshou;
+        ajax({
+            url:"../api/dqshouye.php",
+            async:false,
+            success:function(data){
+                jieshou=data;
+            }
+        });
+        return jieshou;
+    })();
+    
+        console.log(data);
+    var shoplist = document.querySelector('.shop-list');
+    var ul = document.querySelector('.overflow-hide');
+    // console.log(ul)
+    ul.innerHTML = '';
+    ul.innerHTML = data.map(function(item){
+        return`<li data-myid="${item.id}">
+                <a href="#" title="iPhone 6 全网通版" class="anim-left">
+                <img class="get-src" data-src="152" src="${item.imgurl}" width="120" height="120">
+                <div class="diy-tip" style="color:#734e8b">
+                <h3>${item.name}</h3>
+                <p style="color: #555">${item.jianjie}</p>
+                <div data-ppid="49937" class="get-price">￥${item.price}</div>
+                </div>
+                </a>
+        </li>`
+    }).join('');
+    
 });
